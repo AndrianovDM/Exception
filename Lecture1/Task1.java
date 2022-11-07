1928e2baedd9e7e5606cbcbcbdf265a33476a773
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-11-07 16:29:42 +0300

    Lecture 1 finish

--- Lecture1/Task1.java
+++ Lecture1/Task1.java
@@ -1,5 +1,24 @@
 package Lecture1;
 
+import java.io.File;
+
 public class Task1 {
-    
+    public static void main(String[] args) {
+        //System.out.println(getFileSize(new File("123")));
+        System.out.println(divide(10,0));
+    }
+
+    public static int divide(int a1, int a2){
+        if (a2 == 0){
+            throw new RuntimeException("Divide by zero is not permit");
+        }
+        return a1/a2;
+    }
+
+    public static long getFileSize(File file){
+        if(!file.exists()){
+            return -1L;
+        }
+        return file.length();
+    }
 }
