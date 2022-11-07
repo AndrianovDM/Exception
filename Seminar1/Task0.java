0639011dd859d981e6ba660b48af2d4ce8e06af1
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-11-07 16:38:24 +0300

    Seminar 1 finish

---
+++ Seminar1/Task0.java
@@ -0,0 +1,15 @@
+public class Task0 {
+    public static void main(String[] args) {
+        int[] array = {1, 6, 4, 8, 9, 10, 95};
+        System.out.println(getArrayLength(array));
+
+    }
+
+    private static int getArrayLength(int[] array) {
+        int min = 10;
+        if (array.length < min) {
+            return -1;
+        }
+        return array.length;
+    }
+}
