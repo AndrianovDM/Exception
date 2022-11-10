5526d256b11d25c7c0cc64342d382b1346decf92
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-11-10 04:02:34 +0300

    Seminar 2 finish

---
+++ Seminar2/Task3.java
@@ -0,0 +1,68 @@
+/*
+Запишите в файл следующие строки:
+Анна=4
+Елена=5
+Марина=6
+Владимир=?      //8
+Константин=?    //10
+Иван=4
+Реализуйте метод, который считывает данные из файла и сохраняет в двумерный массив (либо HashMap, если студенты с ним знакомы).
+В отдельном методе нужно будет пройти по структуре данных
+если сохранено значение ?, заменить его на соответствующее число.
+Если на каком-то месте встречается символ, отличный от числа или ?, бросить подходящее исключение.
+Записать в тот же файл данные с замененными символами ?.
+ */
+
+package Seminar2;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Task3 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(reader().toString());
+        writer(reader());
+    }
+
+    public static HashMap<String, String> reader() throws IOException {
+        BufferedReader bf = new BufferedReader(new FileReader("C:\\Users\\Dmitry\\Downloads\\Postgraduate studies\\Homework program\\Exception\\Seminar2\\fileForTask3.txt"));
+        
+        String line;
+        HashMap<String, String> hashMap = new HashMap<>();
+        while ((line = bf.readLine()) != null) {
+            String[] tmp = line.split("=");
+            //System.out.println("tmp" + Arrays.toString(tmp));
+            tmp = answerChecker(tmp);
+            hashMap.put(tmp[0], tmp[1]);
+        }
+        return hashMap;
+    }
+
+
+    public static String[] answerChecker(String[] str) {
+        if (str[1].equals("?")) {
+            str[1] = Integer.toString(str[0].length());
+        } else {
+            try {
+                Integer.parseInt(str[1]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                System.out.println("встретился символ, отличный от числа или ?");
+                str[1] = Integer.toString(str[0].length());
+            }
+        }
+        return str;
+    }
+
+    public static void writer(HashMap<String, String> hashMap) throws IOException {
+        BufferedWriter bw = new BufferedWriter(
+                new FileWriter("C:\\Users\\Dmitry\\Downloads\\Postgraduate studies\\Homework program\\Exception\\Seminar2\\fileForTask3_2.txt"));
+        for (String key : hashMap.keySet()) {
+            //System.out.printf("%s = %s", key, hashMap.get(key));
+            bw.write(String.format("%s = %s", key, hashMap.get(key)));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
--- Seminar2/task1Wrong.java
+++ Seminar2/task1Wrong.java
@@ -1,20 +1,17 @@
 package Seminar2;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class task1Write {
+public class task1Wrong {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] arr = new int[10];
         System.out.println("Укажите индекс элемента массива, в который хотите записать значение 1");
-        if (scanner.hasNextInt()) {
-            int index = scanner.nextInt();
-            if (index < arr.length && index >= 0)
-                arr[index] = 1;
-        } else {
-            System.out.println("Некорректный ввод");
+        int index = scanner.nextInt();
+        try {
+            arr[index] = 1;
+        } catch (Exception e) {
+            System.out.println("Указан индекс за пределами массива");
         }
-        System.out.println(Arrays.toString(arr));
     }
 }
